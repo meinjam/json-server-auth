fb742036716bb26315b45bad7036863568f268d3
meinjam <injam.cse@gmail.com>
2024-06-10 20:04:30 +0600

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const rules = auth.rewriter({
+  users: 640,
+  posts: 660,
+});
+
+server.db = router.db;
+
+server.use(middlewares);
+server.use(rules);
+server.use(auth);
+server.use(router);
+server.listen(7500, () => {
+  console.log('JSON Server is running');
+});
